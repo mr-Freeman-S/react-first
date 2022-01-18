3544ff447c7802982901bf0780822cbfa2456cdd
mr-Freeman-S <mr.baranok@mail.ru>
2022-01-18 22:39:56 +0300

    Split into components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 import './App.css';
 
 
@@ -7,25 +10,10 @@ import './App.css';
 const App = () => {
   return (
     <div className='app-wrapper'>
-      <header className='header'>
-        <img src="https://uc.agronews.com/sitelogomain/i/1579001627_21339116715e1da71b0ff535.71571325.png" />
-      </header>
-      <nav className='nav'>
-        <div><a>Profile</a></div>
-        <div><a>Messages</a></div>
-        <div><a>News</a></div>
-        <div><a>Music</a></div>
-        <div><a>Setting</a></div>
-      </nav>
-      <div className='content'>
-        <img src='https://farming.org.ua/files/rye/%D1%82%D0%B5%D1%85%D0%BD%D0%BE%D0%BB%D0%BE%D0%B3%D0%B8%D1%8F%20%D0%B2%D1%8B%D1%80%D0%B0%D1%89%D0%B8%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%20%D0%BE%D0%B7%D0%B8%D0%BC%D0%BE%D0%B9%20%D1%80%D0%B6%D0%B8.jpg'/>
-        <div>ava + description</div>
-        <div>My post</div>
-        <div>Posted
-          <div>post-1</div>
-          <div>post-2</div>
-        </div>
-      </div>
+      <Header />
+      <Navbar />
+      <Profile />
+      
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+        <img src="https://uc.agronews.com/sitelogomain/i/1579001627_21339116715e1da71b0ff535.71571325.png" alt='logo' />
+      </header>
+    );
+}
+
+export default Header;
